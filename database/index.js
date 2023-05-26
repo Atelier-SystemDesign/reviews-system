4b1016ef2786fb09bf96fcd9a04cae235402c3ef
Segue <gencogluseguy@gmail.com>
2023-05-25 19:51:29 -0700

    ETL work for reviews and ratings

--- database/index.js
+++ database/index.js
@@ -1,45 +1,50 @@
-require('dotenv').config();
-const { Client } = require('pg');
+// require('dotenv').config({ path: '../.env' });
+// const { Client } = require('pg');
 
-const client = new Client({
-  user: process.env.USER,
-  host: process.env.HOST,
-  database: process.env.DBNAME,
-  port: process.env.PORT,
-});
+// const client = new Client({
+//   user: process.env.USER,
+//   host: process.env.HOST,
+//   database: process.env.DATABASE_NAME,
+//   port: process.env.PORT,
+//   password: process.env.PASSWORD,
+// });
 
-client.connect()
-  .then(() => client.query(`CREATE TABLE IF NOT EXISTS reviews(
-  id SERIAL PRIMARY KEY,
-  product_id INTEGER REFERENCES products(product_id),
-  rating INTEGER,
-  date TIMESTAMP,
-  summary VARCHAR(255),
-  body VARCHAR(255),
-  recommend BOOLEAN,
-  reported BOOLEAN,
-  reviewer_name VARCHAR(255),
-  reviewer_email VARCHAR(255),
-  response VARCHAR(255),
-  helpfulness INTEGER DEFAULT 0);`))
-  .then(() => client.query(`CREATE TABLE IF NOT EXISTS reviews_photos (
-    id SERIAL PRIMARY KEY,
-    review_id INTEGER REFERENCES reviews,
-    url VARCHAR(255));`))
-  .then(() => client.query(`CREATE TABLE IF NOT EXISTS characteristic_reviews (
-    id SERIAL PRIMARY KEY,
-    characteristic_id INTEGER REFERENCES characteristics(id),
-    review_id INTEGER REFERENCES reviews(id),
-    value INTEGER);`))
-  .then(() => client.query(`CREATE TABLE IF NOT EXISTS characteristics (
-    id SERIAL PRIMARY KEY,
-    product_id INTEGER REFERENCES products(product_id),
-    name VARCHAR(255));`))
-  .then(() => {
-    console.log('tables are created');
-    client.end();
-  })
-  .catch((err) => {
-    console.log('query error', err.message);
-    client.end();
-  });
+// // console.log(process.env);
+// console.log('db', process.env.DATABASE_NAME);
+// client.connect().then(() => client.query(`CREATE TABLE IF NOT EXISTS reviews(
+//   id SERIAL PRIMARY KEY,
+//   product_id INTEGER REFERENCES products(product_id),
+//   rating INTEGER,
+//   date TIMESTAMP,
+//   summary VARCHAR(255),
+//   body VARCHAR(255),
+//   recommend BOOLEAN,
+//   reported BOOLEAN,
+//   reviewer_name VARCHAR(255),
+//   reviewer_email VARCHAR(255),
+//   response VARCHAR(255),
+//   helpfulness INTEGER DEFAULT 0);`))
+//   .then(() => client.query(`CREATE TABLE IF NOT EXISTS products (
+//   product_id SERIAL PRIMARY KEY
+// );`))
+//   .then(() => client.query(`CREATE TABLE IF NOT EXISTS reviews_photos (
+//     id SERIAL PRIMARY KEY,
+//     review_id INTEGER REFERENCES reviews(id),
+//     url VARCHAR(255));`))
+//   .then(() => client.query(`CREATE TABLE IF NOT EXISTS characteristic_reviews (
+//     id SERIAL PRIMARY KEY,
+//     characteristic_id INTEGER REFERENCES characteristics(id),
+//     review_id INTEGER REFERENCES reviews(id),
+//     value INTEGER);`))
+//   .then(() => client.query(`CREATE TABLE IF NOT EXISTS characteristics (
+//     id SERIAL PRIMARY KEY,
+//     product_id INTEGER REFERENCES products(product_id),
+//     name VARCHAR(255));`))
+//   .then(() => {
+//     console.log('tables are created');
+//     client.end();
+//   })
+//   .catch((err) => {
+//     console.log('query error', err.message);
+//     client.end();
+//   });
--- schema.sql
+++ schema.sql
@@ -1,45 +1,61 @@
 DROP TABLE IF EXISTS reviews;
-DROP TABLE IF EXISTS products;
 DROP TABLE IF EXISTS characteristic_reviews;
 DROP TABLE IF EXISTS characteristics;
 DROP TABLE IF EXISTS reviews_photos;
 
-
-
 CREATE TABLE IF NOT EXISTS reviews(
-  id SERIAL PRIMARY KEY,
-  product_id INTEGER REFERENCES products(product_id),
+  id INTEGER PRIMARY KEY,
+  product_id INTEGER,
   rating INTEGER,
-  date TIMESTAMP,
-  summary VARCHAR(255),
-  body VARCHAR(255),
+  date BIGINT,
+  summary TEXT,
+  body TEXT,
   recommend BOOLEAN,
   reported BOOLEAN,
   reviewer_name VARCHAR(255),
   reviewer_email VARCHAR(255),
-  response VARCHAR(255),
+  response TEXT,
   helpfulness INTEGER DEFAULT 0);
 
 
-  CREATE TABLE IF NOT EXISTS products (
-  product_id SERIAL PRIMARY KEY
-);
+CREATE TABLE IF NOT EXISTS characteristics (
+    id INTEGER PRIMARY KEY,
+    product_id INTEGER,
+    name VARCHAR(255));
 
 
 CREATE TABLE IF NOT EXISTS characteristic_reviews (
-    id SERIAL PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     characteristic_id INTEGER REFERENCES characteristics(id),
     review_id INTEGER REFERENCES reviews(id),
-value INTEGER);
+    value INTEGER);
 
-
-CREATE TABLE IF NOT EXISTS characteristics (
-    id SERIAL PRIMARY KEY,
-    product_id INTEGER REFERENCES products(product_id),
-    name VARCHAR(255));
 
 CREATE TABLE IF NOT EXISTS reviews_photos (
-    id SERIAL PRIMARY KEY,
-    review_id INTEGER REFERENCES revies,
+    id INTEGER PRIMARY KEY,
+    review_id INTEGER REFERENCES reviews,
     url VARCHAR(255));
 
+
+/**** COPY STATEMENTS ****/
+
+COPY reviews(id, product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness)
+FROM  '/Users/segue/Desktop/SDC/reviews-system/dataFiles/reviews.csv'
+DELIMITER ',' CSV HEADER;
+
+/**** updating null values ****/
+UPDATE reviews
+SET response= ''
+WHERE response='null';
+
+COPY characteristics(id, product_id, name)
+FROM  '/Users/segue/Desktop/SDC/reviews-system/dataFiles/characteristics.csv'
+DELIMITER ',' CSV HEADER;
+
+COPY characteristic_reviews(id,  characteristic_id, review_id, value)
+FROM  '/Users/segue/Desktop/SDC/reviews-system/dataFiles/characteristic_reviews.csv'
+DELIMITER ',' CSV HEADER;
+
+COPY reviews_photos(id,  review_id, url)
+FROM  '/Users/segue/Desktop/SDC/reviews-system/dataFiles/reviews_photos.csv'
+DELIMITER ',' CSV HEADER;
