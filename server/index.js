8a0c4f78ab5d900733c80c91060791b89d7f0b73
Segue <gencogluseguy@gmail.com>
2023-06-02 09:29:32 -0700

    Preparing for loader.io cloud test

--- server/index.js
+++ server/index.js
@@ -9,11 +9,12 @@ const PORT = process.env.SERVER_PORT;
 const app = express();
 
 // Middleware
-// app.use(morgan('dev')); when testing k6 comment this
+app.use(morgan('dev')); // when testing k6 comment this out
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/api', router);
+app.use(express.static('public')); // anything inside public will be available
 
 app.listen(PORT, () => {
   console.log(`Server available at http://localhost:${PORT}`);
--- server/router/index.js
+++ server/router/index.js
@@ -3,7 +3,6 @@ const { reviews } = require('../controller');
 
 router.get('/reviews', reviews.getReviews);
 router.post('/reviews', reviews.addReviews);
-// router.put('/reviews/helpful/:review_id', reviews.markReviewAsHelpful);
 router.put('/reviews/:review_id/helpful', reviews.markReviewAsHelpful);
 router.put('/reviews/:review_id/report', reviews.reportReview);
 router.get('/reviews/meta', reviews.getReviewsMetaData);
